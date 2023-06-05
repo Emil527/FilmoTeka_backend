5faa8758e0cfca883c025dcfbbd86312fe83a762
Emil527 <emill.mustafin@yandex.ru>
2023-06-05 21:14:53 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+import cors from 'cors';
+import 'dotenv/config';
+import routes from './routes/index.js';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api', routes);
+
+const port = process.env.PORT || 4444;
+
+mongoose
+	.connect(process.env.MONGODB_URL)
+	.then(() => {
+		console.log('Mongodb connected');
+		app.listen(port, () => {
+			console.log(`Server is listening on port ${port}`);
+		});
+	})
+	.catch(err => {
+		console.log({ err });
+		process.exit(1);
+	});
---
+++ routes/user.route.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import { body } from 'express-validator';
+import { handleValidationErrors, checkAuth } from '../utils/index.js';
+import { UserController } from '../controllers/index.js';
+import User from '../models/User.js';
+
+const router = express.Router();
+
+router.post(
+	'/login',
+	body('email').isEmail(),
+	body('password')
+		.isLength({ min: 5 })
+		.withMessage('Пароль должен быть минимум 5 символов'),
+	handleValidationErrors,
+	UserController.login
+);
+
+router.post(
+	'/register',
+	body('email')
+		.isEmail()
+		.withMessage('Неверный формат почты')
+		.custom(async value => {
+			const user = await User.findOne({ email: value });
+			if (user)
+				return Promise.reject('email с таким именем уже существует');
+		}),
+	body('password')
+		.isLength({ min: 5 })
+		.withMessage('Пароль должен быть минимум 5 символов'),
+	body('fullName')
+		.isLength({ min: 3 })
+		.withMessage('Укажите имя минимум 3 символов'),
+	handleValidationErrors,
+	UserController.register
+);
+
+router.get('/me', checkAuth, UserController.getMe);
+
+export default router;
