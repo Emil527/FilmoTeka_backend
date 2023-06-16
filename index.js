ab1ff5ac348e55a4992df4530400b5456e1e5907
Emil527 <emill.mustafin@yandex.ru>
2023-06-17 01:27:38 +0300

    fix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 
 import cors from 'cors';
-import 'dotenv/config';
+import dotenv from 'dotenv';
 import routes from './routes/index.js';
 import mongoose from 'mongoose';
 
 
-require('dotenv').config();
+dotenv.config();
 const app = express();
 
 app.use(express.json());
